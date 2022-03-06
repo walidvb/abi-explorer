5c63cb9b8e6f9335dc18e4bab781baed155c5d45
Walid <hello@walidvb.com>
2022-03-06 18:24:51 -0300

    correct command name

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'abi-explorer.helloWorld',
+    'abi-explorer.exploreABI',
     () => {
       createPreview();
     }
